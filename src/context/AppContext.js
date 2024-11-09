8aa298f8e3b491bb4b0a604a01abde02bc6ac691
dadhichharshit22 <dadhich.harshit222002@gmail.com>
2024-11-09 09:43:11 +0530

    First Commit

---
+++ src/context/AppContext.js
@@ -0,0 +1,29 @@
+import { createContext, useState } from "react";
+
+
+
+export const AppContext = createContext();
+
+function AppContextProvider({children}){
+
+    const [loading,setLoading] = useState(false);
+    const [posts,setPosts] = useState([]);
+    const [page,setPage] = useState(1);
+    const [totalPages,setTotalPages] = useState(null);
+
+    const value = {
+        posts,
+        setPosts,
+        loading,
+        setLoading,
+        page,
+        setPage,
+        totalPages,
+        setTotalPages
+    };
+
+    return <AppContext.Provider value={value}>
+         {children}
+       </AppContext.Provider>
+
+}
